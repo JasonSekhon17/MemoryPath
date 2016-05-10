7e214d643b3f3ec64b7457d13aa80989b1208c12
Michael <mc70050@gmail.com>
2016-05-10 12:34:33 -0700

    implemented generate path(hard code), show path(all at once), reset grid(in an instant), make cells of the table respond to clicks when the game has started.

--- game.php
+++ game.php
@@ -29,7 +29,20 @@
             var stageNumber;                // Represents the current stage number.
             var totalScore;                 // Total score of player for this game.
             var stageScore;                 // Score for current stage.
-            var timer;                      // Placeholder variable to represent timer, acts as an integer
+            var life = 3;                   // Player's life for current stage.
+            var timer = 60;                 // Placeholder variable to represent timer, acts as an integer
+            var pathArray;
+            var playerArray;
+
+            // If game has been started, the cells of the table will
+            // respond to clicks.
+            $(document).on("pagecreate", "#pageone", function () {
+                $("td.panel").click(function () {
+                    if (gameStatus) {
+                        $(this).css("background-color", "yellow");
+                    }
+                });
+            });
 
             // If timer counts down to zero, check if player's life is zero.
             // If life is zero, continue onto gameover process.
@@ -104,8 +117,8 @@
             // This function starts the game.
             function gameAct() {
                 if (!gameStatus) {
-                    startGame();
                     gameStatus = true;
+                    startGame();
                     // Try to make the button disappear. (Not done)
                 }
             }
@@ -123,7 +136,8 @@
             // No validation of game status is needed because this function will
             // only be called when validation is done.
             function generatePath() {
-                // Implementation needed.
+                // hard coded path.
+                window.pathArray = ["3 0", "3 1", "2 1", "1 1", "1 2", "1 3", "2 3", "2 4"];
             }
 
             // This function will use the random path generated in another function 
@@ -132,15 +146,28 @@
             // Time interval between the change of color of each tile depends on
             // the game mode and current stage number.
             function showPath() {
-                // Implementation needed.
-                // Use php inside javascript to find out what the game mode chosen is.
+                for (i = 0; i < window.pathArray.length; i++) {
+                    array = window.pathArray[i];
+                    row = parseInt(array.substring(0, 1));
+                    col = parseInt(array.substring(2, 3));
+                    var table = $("#grid")[0];
+                    var cell = table.rows[row].cells[col];
+                    $(cell).css('background-color', 'white');
+                }
             }
 
             // This function will reset the all the tiles inside the grid to original
             // state, which means original color. The recorded randomized path is not 
             // affected.
             function resetGrid() {
-                // Implementation needed.
+                for (i = 0; i < window.pathArray.length; i++) {
+                    array = window.pathArray[i];
+                    row = parseInt(array.substring(0, 1));
+                    col = parseInt(array.substring(2, 3));
+                    var table = $("#grid")[0];
+                    var cell = table.rows[row].cells[col];
+                    $(cell).css('background-color', '#808080');
+                }
             }
 
             // This function basically makes the timer start counting down.
@@ -161,9 +188,9 @@
         </script>
     </head>
     <body>
-        <div data-role="page">
+        <div data-role="page" id="pageone">
             <div data-role="header">
-                <h1>Time</h1>
+                <h1>Time: <script>document.write(timer);</script></h1>
                 <a href="#game-menu" data-rel="popup" data-transition="slideup" class="ui-btn ui-corner-all ui-btn-inline" data-position-to="window">Menu</a>
                     <div data-role="popup" data-theme="b" class="ui-content ui-corner-all" data-dismissible="false" id="game-menu">
                         <a href="#in-game-instruction" data-rel="popup" data-transition="popup" class="ui-btn ui-corner-all" data-position-to="window">Instruction</a>
@@ -176,7 +203,7 @@
                         $row = 1;
                         $col = 1;
 
-                        echo '<table class="game-panel">';
+                        echo '<table class="game-panel" id="grid">';
                         while ($row <= $startingGridNum) {
                             echo '<tr>';
                             while ($col <= $startingGridNum) {
@@ -191,6 +218,9 @@
                     ?>
                     <a href="#" id="actButton" class="ui-btn ui-corner-all ui-btn-inline" onclick="gameAct()">Start</a>
                 </div>
+            </div>
+            <div data-role="footer">
+                <h3>Life: <script>document.write(life);</script></h3>
             </div>
         </div>
     </body>
